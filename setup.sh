bd3b1eccaa5d70d2120bd43604e66bb10451e430
vigilante <73174517+sunstep@users.noreply.github.com>
2022-10-16 13:52:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/zsh
-
+Echo "Installing dependencies, building and moving binary"
 sudo apt install libc6-dev
 make
 sudo mv endlessh /usr/local/bin
 which endlessh
 
+Echo "Setting up Endlessh" 
 sudo cp util/endlessh.service /etc/systemd/system
 sudo systemctl enable endlessh
 sudo mkdir -p /etc/endlessh
@@ -13,3 +14,10 @@ curl https://raw.githubusercontent.com/sunstep/endlessh/master/config --output c
 sudo systemctl start endlessh
 
 sudo netstat -tulpn | grep endlessh
+
+Echo "Endlessh not bound to port 22, fixing it now."
+cd /etc/systemd/system/
+sudo mv endlessh.service endlessh.service.backup
+curl https://raw.githubusercontent.com/sunstep/endlessh/master/endlessh.service --output endlessh.service
+sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/endlessh
+sudo systemctl daemon-reload && sudo systemctl restart endlessh
