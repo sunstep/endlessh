301ed464b3059085bc516c0c88aa4f055fd726c1
sunstep <73174517+sunstep@users.noreply.github.com>
2023-06-01 20:34:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,23 +7,29 @@ sudo apt install libc6-dev gcc make curl net-tools -y
 make
 
 # Move the endlessh binary to the appropriate location
+echo "Moving endlessh to /usr/local/bin..."
 sudo mv endlessh /usr/local/bin
 
 # Verify the installation
 which endlessh
 
 # Copy the endlessh systemd service file
+echo "Copying endlessh.service to /etc/systemd/system"
 sudo cp endlessh.service /etc/systemd/system
 
 # Enable the endlessh service
+echo "Enabling endlessh..."
 sudo systemctl enable endlessh
 
 # Create the configuration file
+echo "Creating /etc/endlessh directory..."
 sudo mkdir -p /etc/endlessh
 cd /etc/endlessh
+echo "Downloading custom endlessh service..."
 sudo curl https://raw.githubusercontent.com/sunstep/endlessh/master/config --output config
 
 # Start the endlessh service
+echo "Starting Endlessh..."
 sudo systemctl start endlessh
 
 # Check if endlessh is listening on port 22
